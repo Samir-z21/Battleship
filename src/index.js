6e7d57b06c7ada89c2ca21ab85da0d211e75afb0
Samir <samir_zulfiqar@yahoo.com>
2024-01-26 16:45:05 -0500

    built Ship,gameBoard functions and tested

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './home.css';
 
-console.log("Hello World")
+console.log("Hello World");
+//Global value
+const axis = 'x';
 
 class Ship {
     constructor (length, hits, sunk) {
@@ -19,5 +21,65 @@ class Ship {
     }
 }
 
-export default Ship
+const gameBoard = (() => {
+    const gameBoardArray = new Array(100).fill(0);
+    let shipCounter = 1;
+    let shipArray = [];
+    const placeShip = (value) => {
+        switch (shipCounter) {
+            case 1:
+                const carrier = new Ship(5, 0, false);
+                shipArray = getCoordinates(carrier,value)
+                break;
+                
+            case 2: 
+                const battleship = new Ship(4, 0, false);
+                shipArray = getCoordinates(battleship,value)
+                break;
+            
+            case 3:
+                const destroyer = new Ship(3, 0, false);
+                shipArray = getCoordinates(destroyer,value)
+                break;
+
+            case 4: 
+                const submarine = new Ship(3, 0, false);
+                shipArray = getCoordinates(submarine,value)
+                break;
+            
+            case 5: 
+                const patrolBoat = new Ship(2, 0, false);
+                shipArray = getCoordinates(patrolBoat,value)
+                break;
+        }
+
+
+        shipCounter++
+
+        return shipArray 
+    } 
+
+    return {
+        placeShip, 
+
+    }
+})()
+
+function getCoordinates (ship, value) {
+    const shipArray = [];
+    if (axis === 'x') {
+        for (let i = 0; i < ship.length; i++) {
+            shipArray.push(value + i);
+        }
+    } else {
+        for (let i = 0; i < ship.length; i += 10) {
+            shipArray.push(value + i);
+        }
+    }
+
+    return shipArray
+    
+}
+
+export { Ship, gameBoard}
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,24 @@
-import Ship from "./index"
+import {Ship, gameBoard} from "./index";
+
+const carrier = new Ship(5, 0, false);
+
+carrier.hit();
+carrier.hit();
+carrier.hit();
+
+
 
 test('Ship Obj', () => {
-    const carrier = new Ship(5, 0, false)
-    expect(carrier).toEqual({
-        length: 5,
-        hits:0,
-        sunk:false
-    });
+    expect(carrier.isSunk()).toBe(false);
 });
 
+
+test('getting ship location "x axis" ', () => {
+    const axis = "x";
+    expect(gameBoard.placeShip(13)).toEqual([13,14,15,16,17]);
+    expect(gameBoard.placeShip(88)).toEqual([88,89,90,91]);
+    expect(gameBoard.placeShip(95)).toEqual([95,96,97]);
+    expect(gameBoard.placeShip(0)).toEqual([0,1,2]);
+    expect(gameBoard.placeShip(54)).toEqual([54,55]);
+});
 
